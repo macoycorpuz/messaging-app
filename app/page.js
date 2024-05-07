701b19b0bf52e792421656296e0a5392d4528f84
macoycorpuz <macoycorpuz@gmail.com>
2024-05-08 02:22:46 +0900

    redirect to chat and username ref

--- app/page.js
+++ app/page.js
@@ -1,17 +1,30 @@
-export default function Login() {
+'use client'
+import { useRouter } from 'next/navigation'
+import { useRef } from 'react'
+
+export default function Home() {
+  const usernameRef = useRef()
+  const router = useRouter()
+
+  const onSubmit = (e) => {
+    console.log(usernameRef.current.value)
+    router.push('/chat')
+    e.preventDefault()
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="w-full max-w-sm">
-        <h1 className="py-4 text-center text-2xl uppercase">
-          Choose your name
-        </h1>
-        <form className="flex flex-col p-4" action="#" method="POST">
-          <label htmlFor="username">Username</label>
+        <h1 className="py-4 text-center text-2xl uppercase">messaging app</h1>
+        <form className="flex flex-col pb-4" onSubmit={onSubmit}>
+          <label htmlFor="username">Choose your </label>
           <div className="mt-2">
             <input
+              ref={usernameRef}
               id="username"
               name="username"
               required
+              autoComplete="username"
               className="w-full rounded-md p-2 shadow-sm ring-1 ring-inset"
             />
           </div>
